3e48998de387335df4b1341421b872a6c2b103d4
Nithishkumar S <nithishkumar@oasyscybernetics.in>
2025-01-23 17:12:15 +0530

    ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,7 +52,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     }),
     NgbModule,
   ],
-  providers: [TranslateService, { provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [TranslateService],
+  // { provide: LocationStrategy, useClass: HashLocationStrategy }
   bootstrap: [AppComponent],
 })
 export class AppModule { }
